9d9a19089b0cbbf6e1f36c6db8bc0e78fd3e826a
ningmeng <1275899401@qq.com>
2017-09-04 00:21:35 +0800

    启用注销讲师功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -12,20 +12,47 @@ define(['jquery','template','bootstrap'],function($,template){
         //绑定预览的点击事件
         $('.preveiw').click(function(){
             var tcId=$(this).closest('td').attr('data-tcId');
-        $.ajax({
-            type:'get',
-            url:'/api/teacher/view',
-            data:{tc_id:tcId},
+            $.ajax({
+                type:'get',
+                url:'/api/teacher/view',
+                data:{tc_id:tcId},
+                dataType:'json',
+                success:function(data){
+                  console.log(data);
+                 //解析数据，渲染页面
+                 var html=template('modalTpl',data.result);
+                 $("#modalInfo").html(html);
+                 //预览功能
+                 $("#teacherModal").modal();
+                }
+            });
+        });
+        //启用注销功能
+        $('.eod').click(function(){
+            var td=$(this).closest('td');
+            var tcId=td.attr('data-tcId');
+            var tcStatus=td.attr('data-status');
+            var that=this;
+         // console.log(1234);
+         $.ajax({
+            type:'post',
+            url:'/api/teacher/handle',
+            data:{tc_id:tcId,tc_status:tcStatus},
             dataType:'json',
             success:function(data){
-              console.log(data);
-             //解析数据，渲染页面
-             var html=template('modalTpl',data.result);
-             $("#modalInfo").html(html);
-             $("#teacherModal").modal();
+              // console.log(data);
+              td.attr('data-status',data.result.tc_status);
+              if(data.result.tc_status==0){
+                $(that).html('注销');
+              }else {
+                $(that).html('启用');
+              }
             }
-        })
-        })
+
+         })
+
+        });
+
      }
    });
 
