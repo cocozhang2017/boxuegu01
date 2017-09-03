2f33e3419f0094832d86759a576d709166069fb5
ningmeng <1275899401@qq.com>
2017-09-03 23:14:55 +0800

    提交讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
    // alert(123);
    $.ajax({
       type:'get',
@@ -9,7 +9,24 @@ define(['jquery','template'],function($,template){
         //解析数据渲染页面
         var html=template('teacherTpl',{list:data.result});
         $('#teacherInfo').html(html);
+        //绑定预览的点击事件
+        $('.preveiw').click(function(){
+            var tcId=$(this).closest('td').attr('data-tcId');
+        $.ajax({
+            type:'get',
+            url:'/api/teacher/view',
+            data:{tc_id:tcId},
+            dataType:'json',
+            success:function(data){
+              console.log(data);
+             //解析数据，渲染页面
+             var html=template('modalTpl',data.result);
+             $("#modalInfo").html(html);
+             $("#teacherModal").modal();
+            }
+        })
+        })
+     }
+   });
 
-      }
-   })
 });
